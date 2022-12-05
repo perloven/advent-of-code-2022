55969cd869eb7d106785941ae594421160b0f7a9
perloven <per.loven@atg.se>
2022-12-05 06:42:27 +0100

    Prepare day 6

---
+++ src/main/kotlin/se/perloven/aoc2022/day6/Day6.kt
@@ -0,0 +1,18 @@
+package se.perloven.aoc2022.day6
+
+fun main() {
+    println("Part 1: ${Day6.part1()}")
+    println("Part 2: ${Day6.part2()}")
+}
+
+object Day6 {
+
+
+    fun part1(): String {
+        return "unsolved"
+    }
+
+    fun part2(): String {
+        return "unsolved"
+    }
+}
