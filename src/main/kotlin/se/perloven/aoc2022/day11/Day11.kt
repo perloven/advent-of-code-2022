3668644deee639cf4c5d26fc94f1923198814ff0
perloven <loven.per@gmail.com>
2022-12-10 07:33:36 +0100

    Prepare day 11

---
+++ src/main/kotlin/se/perloven/aoc2022/day11/Day11.kt
@@ -0,0 +1,17 @@
+package se.perloven.aoc2022.day11
+
+fun main() {
+    println("Part 1: ${Day11.part1()}")
+    println("Part 2: ${Day11.part2()}")
+}
+
+object Day11 {
+
+    fun part1(): Int {
+        return -1
+    }
+
+    fun part2(): Int {
+        return -1
+    }
+}
