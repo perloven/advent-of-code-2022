8dc25d0f6672b8befecd40153731ab7c94a6d18b
perloven <loven.per@gmail.com>
2022-12-11 07:23:02 +0100

    Day 11 part 2 WIP
    Monkey 4 with operation old * old generates absolutely massive values, which are taking way too long to calculate.

--- src/main/kotlin/se/perloven/aoc2022/day11/MonkeyInTheMiddle.kt
+++ src/main/kotlin/se/perloven/aoc2022/day11/MonkeyInTheMiddle.kt
@@ -1,6 +1,9 @@
 package se.perloven.aoc2022.day11
 
 import se.perloven.aoc2022.util.ResourceFiles
+import java.math.BigInteger
+import java.time.Duration
+import java.time.Instant
 
 fun main() {
     println("Part 1: ${MonkeyInTheMiddle.part1()}")
@@ -9,18 +12,18 @@ fun main() {
 
 object MonkeyInTheMiddle {
 
-    private data class Monkey(val id: Int, val operation: (Int) -> Int, val testDenominator: Int, val trueTarget: Int,
-                              val falseTarget: Int, val items: MutableList<Item>, var inspections: Int = 0) {
+    private data class Monkey(val id: Int, val operation: (BigInteger) -> BigInteger, val testDenominator: Int, val trueTarget: Int,
+                              val falseTarget: Int, val items: MutableList<Item>, var inspections: BigInteger = BigInteger.ZERO) {
         fun inspect(): List<ThrownItem> {
             val itemsToThrow = items.toList()
-            inspections += items.size
-            items.forEach { it.worryLevel = operation(it.worryLevel) / 3 }
+            inspections = inspections.add(BigInteger.valueOf(items.size.toLong()))
+            items.forEach { it.worryLevel = operation(it.worryLevel) }
             items.clear()
             return itemsToThrow.map { ThrownItem(target = decideTarget(it), item = it) }
         }
 
         private fun decideTarget(item: Item): Int {
-            return if (item.worryLevel % testDenominator == 0) {
+            return if (item.worryLevel.rem(BigInteger.valueOf(testDenominator.toLong())) == BigInteger.ZERO) {
                 trueTarget
             } else {
                 falseTarget
@@ -32,14 +35,14 @@ object MonkeyInTheMiddle {
         }
     }
 
-    private data class Item(var worryLevel: Int)
+    private data class Item(var worryLevel: BigInteger)
 
     private data class ThrownItem(val target: Int, val item: Item)
 
-    fun part1(): Int {
+    fun part1(): String {
         val monkeys = parseMonkeys()
         doMonkeyBusiness(monkeys)
-        return calculateLevelOfMonkeyBusiness(monkeys)
+        return calculateLevelOfMonkeyBusiness(monkeys).toString()
     }
 
     private fun parseMonkeys(): List<Monkey> {
@@ -62,42 +65,50 @@ object MonkeyInTheMiddle {
             items = sixLineList[1]
                 .drop(2)
                 .map { it.replace(",", "") }
-                .map { Item(worryLevel = it.toInt()) }
+                .map { Item(worryLevel = BigInteger.valueOf(it.toLong())) }
                 .toMutableList()
         )
     }
 
-    private fun parseOperation(monkeyIndex: Int): (Int) -> Int {
+    private fun parseOperation(monkeyIndex: Int): (BigInteger) -> BigInteger {
         return when(monkeyIndex) {
-            0 -> { old -> old * 3 }
-            1 -> { old -> old * 17 }
-            2 -> { old -> old + 1}
-            3 -> { old -> old + 2 }
-            4 -> { old -> old * old }
-            5 -> { old -> old + 8 }
-            6 -> { old -> old + 6 }
-            7 -> { old -> old + 7 }
+            0 -> { old -> old.multiply(BigInteger.valueOf(3)) }
+            1 -> { old -> old.multiply(BigInteger.valueOf(17)) }
+            2 -> { old -> old.add(BigInteger.valueOf(1)) }
+            3 -> { old -> old.add(BigInteger.valueOf(2)) }
+            4 -> { old ->
+                val start = Instant.now()
+                val result = old.pow(2)
+                val powDuration = Duration.between(start, Instant.now())
+                println("pow duration: ${powDuration.toMillis()}, result: $result")
+                result
+            }
+            5 -> { old -> old.add(BigInteger.valueOf(8)) }
+            6 -> { old -> old.add(BigInteger.valueOf(6)) }
+            7 -> { old -> old.add(BigInteger.valueOf(7)) }
             else -> throw IllegalArgumentException("Invalid monkey index $monkeyIndex")
         }
     }
 
     private fun doMonkeyBusiness(monkeys: List<Monkey>) {
-        repeat(20) {
+        repeat(10_000) { i ->
+            println(i)
             for (monkey in monkeys) {
+                println("monkey ${monkey.id}")
                 val itemsToThrow = monkey.inspect()
-                itemsToThrow.forEach {
-                    val targetMonkey = monkeys[it.target]
-                    targetMonkey.receiveThrownItem(it.item)
+                itemsToThrow.forEach { thrownItem ->
+                    val targetMonkey = monkeys[thrownItem.target]
+                    targetMonkey.receiveThrownItem(thrownItem.item)
                 }
             }
         }
     }
 
-    private fun calculateLevelOfMonkeyBusiness(monkeys: List<Monkey>): Int {
+    private fun calculateLevelOfMonkeyBusiness(monkeys: List<Monkey>): BigInteger {
         return monkeys.map { it.inspections }
             .sortedByDescending { it }
             .take(2)
-            .reduce { acc: Int, i: Int -> acc * i }
+            .reduce { acc: BigInteger, i: BigInteger -> acc.multiply(i) }
     }
 
     fun part2(): Int {
