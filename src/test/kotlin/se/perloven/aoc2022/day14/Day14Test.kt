f87dc3e9b94ddbc38b7f5ea314e693c249833ac9
perloven <loven.per@gmail.com>
2022-12-13 20:08:23 +0100

    Prepare day 14

---
+++ src/test/kotlin/se/perloven/aoc2022/day14/Day14Test.kt
@@ -0,0 +1,17 @@
+package se.perloven.aoc2022.day14
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day14Test {
+
+    @Test
+    fun part1() {
+        assertEquals(-1, Day14.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(-2, Day14.part2())
+    }
+}
