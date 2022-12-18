4ac7a28269c828b7f038b48f2c44fca6b9f1eae1
perloven <loven.per@gmail.com>
2022-12-18 07:45:45 +0100

    Prepare day 19

---
+++ src/main/kotlin/se/perloven/aoc2022/day19/Day19.kt
@@ -0,0 +1,17 @@
+package se.perloven.aoc2022.day19
+
+fun main() {
+    println("Part 1: ${Day19.part1()}")
+    println("Part 2: ${Day19.part2()}")
+}
+
+object Day19 {
+
+    fun part1(): Int {
+        return -1
+    }
+
+    fun part2(): Int {
+        return -2
+    }
+}
---
+++ src/test/kotlin/se/perloven/aoc2022/day19/Day19Test.kt
@@ -0,0 +1,18 @@
+package se.perloven.aoc2022.day19
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day19Test {
+
+    @Test
+    fun part1() {
+        assertEquals(-1, Day19.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(-2, Day19.part2())
+    }
+}
