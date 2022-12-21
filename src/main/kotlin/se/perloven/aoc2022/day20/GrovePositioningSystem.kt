0d9dd6dd0b787892ace7d66d90f552ee78cf27ac
perloven <loven.per@gmail.com>
2022-12-21 20:20:02 +0100

    Prepare day 20 - Grove Positioning System

---
+++ src/main/kotlin/se/perloven/aoc2022/day20/GrovePositioningSystem.kt
@@ -0,0 +1,17 @@
+package se.perloven.aoc2022.day20
+
+fun main() {
+    println("Part 1: ${GrovePositioningSystem.part1()}")
+    println("Part 2: ${GrovePositioningSystem.part2()}")
+}
+
+object GrovePositioningSystem {
+
+    fun part1(): Int {
+        return -1
+    }
+
+    fun part2(): Int {
+        return -2
+    }
+}
---
+++ src/test/kotlin/se/perloven/aoc2022/day20/GrovePositioningSystemTest.kt
@@ -0,0 +1,17 @@
+package se.perloven.aoc2022.day20
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class GrovePositioningSystemTest {
+
+    @Test
+    fun part1() {
+        assertEquals(-1, GrovePositioningSystem.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(-2, GrovePositioningSystem.part2())
+    }
+}
