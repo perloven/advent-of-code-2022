7883f041d2c0e1e64134417e5b3e93e0a9dbf441
perloven <per.loven@atg.se>
2022-12-21 07:45:55 +0100

    Prepare day 21

---
+++ src/main/kotlin/se/perloven/aoc2022/day21/MonkeyMatch.kt
@@ -0,0 +1,17 @@
+package se.perloven.aoc2022.day21
+
+fun main() {
+    println("Part 1: ${MonkeyMatch.part1()}")
+    println("Part 2: ${MonkeyMatch.part2()}")
+}
+
+object MonkeyMatch {
+
+    fun part1(): Long {
+        return -1
+    }
+
+    fun part2(): Long {
+        return -2
+    }
+}
---
+++ src/test/kotlin/se/perloven/aoc2022/day21/MonkeyMatchTest.kt
@@ -0,0 +1,18 @@
+package se.perloven.aoc2022.day21
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MonkeyMatchTest {
+
+    @Test
+    fun part1() {
+        assertEquals(-1, MonkeyMatch.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(-2, MonkeyMatch.part2())
+    }
+}
